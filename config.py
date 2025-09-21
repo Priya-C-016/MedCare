8d4952a5635cf219e08e28b63d0ab41e1584ea50
Priya Chanchal <145918494+Priya-C-016@users.noreply.github.com>
2025-09-21 22:54:35 +0530

    Initial Commit

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 firebase_config = {
     "apiKey": "AIzaSyBqaw_dEdlwx2-faCOnbkS-jzssG_R5qmE",
     "authDomain": "medcareapp-326f5.firebaseapp.com",
-    "databaseURL": "https://medcareapp-326f5.firebaseio.com",  # optional, used by Pyrebase
+    "databaseURL": "https://medcareapp-326f5.firebaseio.com",  # optional
     "projectId": "medcareapp-326f5",
     "storageBucket": "medcareapp-326f5.firebasestorage.app",
     "messagingSenderId": "814156594922",
